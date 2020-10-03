bcc9e47d0f1567d9d8da9fd030821ba9318dc5e3
Luke0195 <lucas201273@gmail.com>
2020-10-03 09:33:38 -0300

    chore: add .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/jsx-filename-extension': 'off'
+
+  },
+};
--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 import './App.css';
-function App(){
-  return(
-    <h1>  Desenvolvimento de uma aplicação utilizando node e React JS</h1>
-  )
+
+function App() {
+  return (
+    <h1>  Teste </h1>
+  );
 }
 
 export default App;
