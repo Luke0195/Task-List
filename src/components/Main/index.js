d304d0cebecfee9a676f9750879ebaa17feaf2d5
Luke0195 <lucas201273@gmail.com>
2020-10-03 11:04:20 -0300

    refactor: estilazando o form

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
 import './styles.css';
+import { FaPlus } from 'react-icons/fa';
 
 export default class Main extends Component {
+  state ={
+    novaTarefa: '',
+  }
+
+  handleChangeInput = (event) => {
+    this.setState({
+      novaTarefa: event.target.value,
+    });
+  }
+
   render() {
+    const { novaTarefa } = this.state;
     return (
       <div className="main">
-        <h1>Lista de Tarefas</h1>
-        <form action="#">
-          <input type="text" name="msg" placeholder="Informe a Tarefa" onChange={this.handleChangeInput} />
-          <button type="submit"> Enviar</button>
+        <h1> Lista de Tarefas</h1>
+        <form action="#" className="form">
+          <input
+            type="text"
+            name="msg"
+            placeholder="Informe a Tarefa"
+            onChange={this.handleChangeInput}
+            value={novaTarefa}
+          />
+
+          <button type="submit">
+            <FaPlus />
+          </button>
         </form>
       </div>
     );
